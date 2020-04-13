06d27244fbb2ecf1ef326e78f17197a890bf2800
CDani11 <cdanitg@gmail.com>
2020-04-12 23:02:31 -0500

    Implementacion de la clase book.

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -8,25 +8,13 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            var numbers = new[] {12.7, 10.3, 6.11, 4.1};
-            var grades = new List<double>() {12.7, 10.3, 6.11, 4.1};
-            var result = 0.0;
+            var book = new Book("Libro de calificaciones de Carlos");
 
-            grades.Add(56.1);
-
-            foreach (var number in grades)
-            {
-                result += number;
-            }
-
-            result /= grades.Count;
-
-            WriteLine($"El promedio de las calificaciones es {result:N3}");
-
-            if (args.Length > 0)
-                WriteLine($"Hello {args[0]}!");
-            else
-                WriteLine("Hello!");
+            book.AddGrade(88.6);
+            book.AddGrade(44.05);
+            book.AddGrade(12.81);
+            book.AddGrade(56.1);
+            book.ShowStatistics();
         }
     }
 }
